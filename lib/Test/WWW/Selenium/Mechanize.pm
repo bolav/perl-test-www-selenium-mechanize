914bd120d7a40ae54b8feff5b960621b6956bc89
bolav <bo@startsiden.no>
2011-12-09 09:32:52 +0700

    Starting refactoring

--- lib/Test/WWW/Selenium/Mechanize.pm
+++ lib/Test/WWW/Selenium/Mechanize.pm
@@ -6,19 +6,22 @@ use Test::More;
 use Test::WWW::Mechanize;
 use utf8;
 
+use HTML::Strip;
 use HTML::TreeBuilder;
 use HTML::TreeBuilder::XPath;
+use HTML::Selector::XPath;
 
 use Data::Dump qw/dump/;
 
 has 'testbuilder' => (is => 'ro', default => sub { Test::More->builder; });
 has 'mech'        => (is => 'ro', default => sub { Test::WWW::Mechanize->new(); });
 
-
 # State variables. NOT THREADSAFE!
 has 'changed'   => (is => 'rw', isa => 'Bool', default => 1);
 has 'wanttree'  => (is => 'rw', isa => 'Bool', default => 0);
 has 'wantxpath' => (is => 'rw', isa => 'Bool', default => 0);
+
+has 'skiptest'  => (is => 'rw', isa => 'Bool', default => 0);
 
 sub run {
     my ($self, $test) = @_;
@@ -40,6 +43,8 @@ sub run {
                 $self->changed(0);
                 $tree && $tree->delete;
                 $tree = undef;
+                $xpath && $xpath->delete;
+                $xpath = undef;
             }
             if ($self->wanttree && !$tree) {
                 $tree = HTML::TreeBuilder->new_from_content($mech->content);
@@ -47,9 +52,17 @@ sub run {
             if ($self->wantxpath && !$xpath) {
                 $xpath = HTML::TreeBuilder::XPath->new_from_content($mech->content);
             }
+            if ($self->skiptest) {
+                $cmd = '$tb->diag("Skipping javascript test");'."\n";
+            }
+
             eval $cmd;
+            if ($@) {
+                die $@.' '.join(' ', @{$command->values})."\n".$cmd;
+            }
         }
-        # $tree && $tree->delete;
+        $tree && $tree->delete;
+        $xpath && $xpath->delete;
     }
 }
 
@@ -95,8 +108,27 @@ sub assertTextPresent {
     return '$mech->text_contains('._esc_in_q($values->[1]).', '.$instr.');'."\n";
 }
 
+sub assertText {
+    my ($self, $tc, $values, $instr) = @_;
+    if (0) {
+        
+    }
+    elsif ($values->[1] =~ m{^//}) {
+        $self->wantxpath(1);
+        # TODO: Filter out HTML
+        return 'is(html_strip($xpath->findnodes_as_string('._esc_in_q($values->[1]).')), '._esc_in_q($values->[2]).', '.$instr.')'."\n";
+    }
+    return '$tb->todo_skip('.$instr.');'."\n";
+}
+
 sub comment {
     my ($self, $tc, $values, $instr) = @_;
+    if ($values->[1] eq 'no_mech') {
+        $self->skiptest(1);
+    }
+    else {
+        $self->skiptest(0);
+    }
     return '$tb->diag('.$instr.');'."\n";
 }
 
@@ -109,6 +141,7 @@ sub waitForTitle {
 
 sub assertElementPresent {
     my ($self, $tc, $values, $instr) = @_;
+    return 'ok('.$self->locator_to_perl($values->[1]).', '.$instr.');'."\n";
     if ($values->[1] =~ /^id=(.*)/) {
         my $val = $1;
         $self->wanttree(1);
@@ -117,12 +150,60 @@ sub assertElementPresent {
     elsif ($values->[1] =~ /^link=(.*)/) {
         return 'ok($mech->find_link( text => '._esc_in_q($1).'), '.$instr.');'."\n";
     }
+    elsif ($values->[1] =~ /^class=(.*)/) {
+        my $val = $1;
+        $self->wanttree(1);
+        return 'ok($tree->look_down("class" => '._esc_in_q($val).'), '.$instr.');'."\n";
+    }
     elsif ($values->[1] =~ m{^//}) {
         $self->wantxpath(1);
         return 'ok($xpath->findnodes('._esc_in_q($values->[1]).')->size, '.$instr.')'."\n";
     }
     else {
         return '$tb->todo_skip('.$instr.');'."\n";
+    }
+}
+
+sub assertElementNotPresent {
+    my ($self, $tc, $values, $instr) = @_;
+    if (0) {
+        
+    }
+    elsif (($values->[1] =~ m{^//}) && ($values->[2])) {
+        $self->wantxpath(1);
+        return 'isnt($xpath->findnodes_as_string('._esc_in_q($values->[1]).'), '._esc_in_q($values->[2]).','.$instr.')'."\n";
+    }
+    elsif ($values->[1] =~ m{^//}) {
+        $self->wantxpath(1);
+        return 'ok(!$xpath->findnodes('._esc_in_q($values->[1]).')->size, '.$instr.')'."\n";
+    }
+}
+
+sub locator_to_perl {
+    my ($self, $locator, $value) = @_;
+    if (!$value) {
+        if ($locator =~ /^id=(.*)/) {
+            my $val = $1;
+            $self->wanttree(1);
+            return '$tree->look_down("id" => '._esc_in_q($val).')';
+        }
+        elsif ($locator =~ /^class=(.*)/) {
+            my $val = $1;
+            $self->wanttree(1);
+            return '$tree->look_down("class" => '._esc_in_q($val).')';
+        }
+        elsif ($locator =~ /^link=(.*)/) {
+            return '$mech->find_link( text => '._esc_in_q($1).')';
+        }
+        elsif ($locator =~ /^css=(.*)/) {
+            my $xp = HTML::Selector::XPath::selector_to_xpath($1);
+            $self->wantxpath(1);
+            return '$xpath->findnodes('._esc_in_q($xp).')->size';
+        }
+        elsif ($locator =~ m{^//}) {
+            $self->wantxpath(1);
+            return '$xpath->findnodes('._esc_in_q($locator).')->size';
+        }
     }
 }
 
@@ -134,8 +215,19 @@ Escape in quotes
 
 sub _esc_in_q {
     my ($str) = @_;
-    $str =~ s/\'//g;
+    $str =~ s/\\/\\\\/g;
+    $str =~ s/\'/\\\'/g;
     return "'".$str."'";
+}
+
+sub html_strip {
+    my ($str) = @_;
+    my $hs = HTML::Strip->new();
+    my $clean_text = $hs->parse( $str );
+    $hs->eof;
+    $clean_text =~ s/\s$//;
+    $clean_text =~ s/^\s//;
+    return $clean_text;
 }
 
 1;
--- t/01.run.t
+++ t/01.run.t
@@ -2,6 +2,7 @@ use strict;
 use Test::More;
 
 use Test::WWW::Selenium::Mechanize;
+use Test::MockModule;
 
 use lib 't/';
 use TestServer;
@@ -11,12 +12,15 @@ my $pid = $server->background();
 
 my $root             = $server->root;
 
-my $signal = ($^O eq 'MSWin32') ? 9 : 15;
-my $nprocesses = kill $signal, $pid;
-is( $nprocesses, 1, 'Signaled the child process' );
-
+my $module = new Test::MockModule('Parse::Selenese::TestCase');
+$module->mock('base_url', sub { $root });
 
 my $twsm = Test::WWW::Selenium::Mechanize->new;
 
 $twsm->run('t/data/test.html');
+
+my $signal = ($^O eq 'MSWin32') ? 9 : 15;
+my $nprocesses = kill $signal, $pid;
+is( $nprocesses, 1, 'Signaled the child process' );
+
 done_testing();
